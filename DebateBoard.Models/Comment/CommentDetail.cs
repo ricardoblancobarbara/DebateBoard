a40e86f27a86390b52b0f02edd367334caa1a5c4
Ricardo <ricardoblancobarbara@gmail.com>
2021-01-28 14:33:44 -0500

    More styling added

--- DebateBoard.Models/Comment/CommentDetail.cs
+++ DebateBoard.Models/Comment/CommentDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,18 @@ namespace DebateBoard.Models
 
         public string Content { get; set; }
 
+        [Display(Name = "Author")]
         public string Id { get; set; }
 
+        [Display(Name = "Article")]
         public int ArticleId { get; set; }
 
         public int Points { get; set; }
 
+        [Display(Name = "Created")]
         public DateTimeOffset CreatedUtc { get; set; }
 
+        [Display(Name = "Modified")]
         public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
